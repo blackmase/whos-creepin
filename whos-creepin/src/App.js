2d23813c7cd4fd89d73aeb0991eeda4b8c66687c
Mason Davis <1mason.davis@gmail.com>
2020-04-20 12:57:48 -1000

    React component

--- whos-creepin/src/App.js
+++ whos-creepin/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      view: {
+        page: 'home',
+        pageTitle: 'Creepin'
+      },
+      formInputs: {
+        name: null,
+        email: null,
+        comment: null,
+        id: null
+      }
+    }
+  }
 }
 
 export default App;
