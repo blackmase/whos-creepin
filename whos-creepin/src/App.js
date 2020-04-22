b85a232f1bcb98080a70e013d5cb61233c457188
Mason Davis <1mason.davis@gmail.com>
2020-04-22 09:37:05 -1000

    complete now adding 3rd party API

--- whos-creepin/src/App.js
+++ whos-creepin/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header.js";
+import Aside from "./components/aside.js";
+import Main from "./components/main.js";
 
 class App extends React.Component {
   constructor(props){
@@ -16,6 +19,53 @@ class App extends React.Component {
         id: null
       }
     }
+  }
+  handleView = (view, postData) => {
+    let pageTitle= '';
+    let formInputs = {
+      name: '',
+      email: '',
+      comment: '',
+      id: null
+    }
+    switch(view) {
+      case 'home':
+      pageTitle = 'Creepin'
+      break
+      case 'addPost':
+      pageTitle = 'Tell me about a Creeper'
+      break
+      case 'editPost':
+      pageTitle = 'Edit this creeper info'
+      formInputs = {
+        name: postData.name,
+        email: postData.email,
+        comment: postData.comment,
+        id: postData.id
+      }
+      break
+      default:
+      break
+    }
+    this.setState({
+      view: {
+        page: view,
+        pageTitle: pageTitle
+      },
+      formInputs: formInputs
+    })
+  }
+  render() {
+    return (
+      <div>
+      <Header/>
+      <Aside handleView={this.handleView}/>
+      <Main
+      handleView={this.handleView}
+      view={this.state.view}
+      formInputs={this.state.formInputs}/>
+      </div>
+    )
   }
 }
 
